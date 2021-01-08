6ced0cfde660d46fe805223a25632682a5a1054b
bart.korn <bartlomiej.kornowski@gmail.com>
2021-01-08 07:14:08 +0100

    #1 Support for displaying current location weather

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,12 +15,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy()
   })
 
-  it(`should have as title 'local-weather-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    const app = fixture.componentInstance
-    expect(app.title).toEqual('local-weather-app')
-  })
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent)
     fixture.detectChanges()
