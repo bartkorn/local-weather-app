e4f3d068913445af00431895732a76c5b21361d7
bart.korn <bartlomiej.kornowski@gmail.com>
2021-01-21 22:07:43 +0100

    Style and lint fixes

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,19 +1,14 @@
-import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule } from '@angular/material/card';
+import { NgModule } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatCardModule } from '@angular/material/card'
+import { MatIconModule } from '@angular/material/icon'
+import { MatToolbarModule } from '@angular/material/toolbar'
 
-const modules = [
-  MatButtonModule,
-  MatToolbarModule,
-  MatIconModule,
-  MatCardModule
- ]
+const modules = [MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule]
 
 @NgModule({
   declarations: [],
   imports: modules,
-  exports: modules
+  exports: modules,
 })
-export class MaterialModule { }
+export class MaterialModule {}
